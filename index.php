4e3f39c1501f1f29db358ab165524fe358f75267
Belikhun <belivipro9x99@gmail.com>
2019-05-23 17:43:02 +0700

    edit version string and typo

--- index.php
+++ index.php
@@ -45,6 +45,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+        <!-- Thay đổi tiêu đề trang hiện đã có trong phần Admin Control Panel -->
         <title><?php print $config["pagetitle"]; ?></title>
 
         <!-- Library First -->
