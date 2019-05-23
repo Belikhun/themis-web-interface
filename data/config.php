0d6d6d48587336d20952b09fbf968c957a54e1c2
Belikhun <belivipro9x99@gmail.com>
2019-05-23 20:31:13 +0700

    edit version string

--- data/config.php
+++ data/config.php
@@ -13,7 +13,7 @@
 	define("APPNAME", "Themis Web Interface");
 	define("AUTHOR", "Belikhun");
 	define("VERSION", "0.4.2");
-	define("VERSION_TAG", "indev");
+	define("VERSION_TAG", "release");
 	define("REPO_ADDRESS", "https://github.com/belivipro9x99/themis-web-interface");
 	define("REPORT_ERROR", REPO_ADDRESS . "/issues");
 
