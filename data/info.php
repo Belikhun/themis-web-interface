eb51cf080b796b955b5a47d94ef416c6be89a3e0
Belikhun <belivipro9x99@gmail.com>
2019-09-01 22:53:16 +0700

    edit version string

--- data/info.php
+++ data/info.php
@@ -9,7 +9,7 @@
 	// dont claim it for your own. thats not nice
 	define("APPNAME", "Themis Web Interface");
 	define("AUTHOR", "Belikhun");
-	define("VERSION", "0.4.7");
+	define("VERSION", "0.5.0");
 	define("VERSION_TAG", "indev");
 	define("REPO_ADDRESS", "https://github.com/belivipro9x99/themis-web-interface");
 	define("REPORT_ERROR", REPO_ADDRESS . "/issues");
