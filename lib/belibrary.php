daf0b7fb5c3d20db2a3331d6764f11d7daf5aee5
Belikhun <belivipro9x99@gmail.com>
2019-09-03 19:41:36 +0700

    fix undefined function "getallheaders" for php-fpm and nginx

--- lib/belibrary.php
+++ lib/belibrary.php
@@ -27,6 +27,18 @@
     if (!isset($_SESSION["username"]))
         $_SESSION["username"] = null;
 
+    if (!function_exists("getallheaders")) {
+        function getallheaders() {
+            $headers = [];
+
+            foreach ($_SERVER as $name => $value)
+                if (substr($name, 0, 5) == "HTTP_")
+                    $headers[str_replace(" ", "-", ucwords(strtolower(str_replace("_", " ", substr($name, 5)))))] = $value;
+
+            return $headers;
+        }
+    }
+
     function isLogedIn() {
         if (session_status() !== PHP_SESSION_NONE && (isset($_SESSION["username"]) || $_SESSION["username"] !== null))
             return true;
