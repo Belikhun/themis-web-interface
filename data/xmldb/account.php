190fa0fe76f477e221d9480fbfd8bb59eb40dbb0
Belikhun <belivipro9x99@gmail.com>
2019-09-04 23:18:37 +0700

    add addUser and deleteUser function

--- data/xmldb/account.php
+++ data/xmldb/account.php
@@ -8,79 +8,188 @@
 
     require_once $_SERVER["DOCUMENT_ROOT"] ."/lib/belibrary.php";
 
-    $dom = new DOMDocument();
-    $dom -> load($_SERVER["DOCUMENT_ROOT"] ."/data/xmldb/account.xml");
-    $root = $dom -> documentElement;
+    $accountDOM = new DOMDocument();
+    $accountDOM -> formatOutput = true;
+    $accountDOM -> preserveWhiteSpace = false;
 
-    $row = $root -> getElementsByTagName("Row");
+    $accountDOM -> load($_SERVER["DOCUMENT_ROOT"] ."/data/xmldb/account.xml");
+    $accountDocument = $accountDOM -> documentElement;
+    $accountTable = $accountDocument -> getElementsByTagName("Table")[0];
+    $accountRow = $accountTable -> getElementsByTagName("Row");
 
-    $acchead = Array();
-    $accdata = Array();
+    define("ACCOUNT_PARSE_KEYPOS", 1);
+    $accountHeader = Array();
+    $accountData = Array();
 
-    foreach($row as $i => $cell) {
-        $cell = $cell -> getElementsByTagName("Data");
-        foreach ($cell as $j => $celldata) {
-            $d[$j] = $celldata -> textContent;
+    foreach ($accountRow as $i => $row) {
+        $cell = $row -> getElementsByTagName("Data");
+        $rowData = Array();
+        
+        foreach ($cell as $j => $cellData) {
+            $tmp = $cellData -> textContent;
+
             if ($i === 0) {
-                $acchead[$j] = $d[$j];
+                $accountHeader[$j] = $tmp;
                 continue;
             }
-            $accdata[$i][$acchead[$j]] = $d[$j];
+
+            if ($j === ACCOUNT_PARSE_KEYPOS && empty($tmp)) {
+                $rowData = Array();
+                break;
+            }
+
+            $rowData[$accountHeader[$j]] = $tmp;
         }
+
+        if (!empty($rowData))
+            $accountData[$rowData[$accountHeader[ACCOUNT_PARSE_KEYPOS]]] = $rowData;
     }
 
     define("LOGIN_SUCCESS", 0);
     define("LOGIN_USERNAMENOTFOUND", 1);
     define("LOGIN_WRONGPASSWORD", 2);
 
-    function simpleLogin($username, $password) {
-        global $accdata;
-        foreach ($accdata as $col)
-            if ($col["username"] === $username)
-                if ($col["password"] === $password || password_verify($password, $col["password"]))
-                    return LOGIN_SUCCESS;
-                else
-                    return LOGIN_WRONGPASSWORD;
-        return LOGIN_USERNAMENOTFOUND;
+    function simpleLogin(String $username, String $password) {
+        global $accountData;
+
+        if (!isset($accountData[$username]))
+            return LOGIN_USERNAMENOTFOUND;
+
+        if ($accountData[$username]["password"] === $password || password_verify($password, $accountData[$username]["password"]))
+            return LOGIN_SUCCESS;
+        else
+            return LOGIN_WRONGPASSWORD;
     }
 
-    function getUserData($username) {
-        global $accdata;
-        foreach ($accdata as $col)
-            if ($col["username"] === $username)
-                return $col;
-                
-        return null;
+    function getUserData(String $username) {
+        global $accountData;
+        return isset($accountData[$username]) ? $accountData[$username] : null;
     }
 
     define("USER_EDIT_SUCCESS", 0);
     define("USER_EDIT_WRONGUSERNAME", 1);
     define("USER_EDIT_ERROR", 2);
 
-    function editUser($username, $replace = Array()) {
-        global $dom;
-        global $row;
-        global $acchead;
+    function editUser(String $username, Array $replace = Array()) {
+        global $accountDOM;
+        global $accountRow;
+        global $accountHeader;
+        global $accountData;
 
-        foreach($row as $i => $cell) {
+        foreach ($accountRow as $i => $row) {
             if ($i === 0)
                 continue;
                 
-            $cell = $cell -> getElementsByTagName("Data");
-            foreach ($cell as $j => $celldata)
-                if ($j === array_search("username", $acchead) && $celldata -> textContent === $username) {
-                    foreach ($replace as $k => $val)
-                        if (($cellindex = array_search($k, $acchead)) !== false)
-                            $cell -> item($cellindex) -> textContent = $val;
+            $cell = $row -> getElementsByTagName("Data");
 
-                    if ($dom -> save($_SERVER["DOCUMENT_ROOT"] ."/data/xmldb/acctmp.xml") === false)
+            foreach ($cell as $j => $cellData)
+                if ($j === array_search("username", $accountHeader) && $cellData -> textContent === $username) {
+                    foreach ($replace as $k => $val)
+                        if (($cellIndex = array_search($k, $accountHeader)) !== false)
+                            $cell -> item($cellIndex) -> textContent = $val;
+
+                    // Update change to parsed data
+                    $accountData = array_merge($accountData, $replace);
+
+                    if ($accountDOM -> save($_SERVER["DOCUMENT_ROOT"] ."/data/xmldb/acctmp.xml") === false)
                         return USER_EDIT_ERROR;
                         
-                    //copy($_SERVER["DOCUMENT_ROOT"] ."/api/xmldb/acctmp.xml", $_SERVER["DOCUMENT_ROOT"] ."/api/xmldb/account.xml");
-                    //unlink($_SERVER["DOCUMENT_ROOT"] ."/api/xmldb/acctmp.xml");
                     rename($_SERVER["DOCUMENT_ROOT"] ."/data/xmldb/acctmp.xml", $_SERVER["DOCUMENT_ROOT"] ."/data/xmldb/account.xml");
                     return USER_EDIT_SUCCESS;
                 }
         }
+
+        return USER_EDIT_WRONGUSERNAME;
+    }
+
+    define("USER_ADD_SUCCESS", 0);
+    define("USER_ADD_USEREXIST", 1);
+    define("USER_ADD_ERROR", 2);
+
+    function addUser(String $id, String $username, String $password, String $name) {
+        global $accountDOM;
+        global $accountTable;
+        global $accountHeader;
+        global $accountRow;
+        global $accountData;
+
+        if (isset($accountData[$username]))
+            return USER_ADD_USEREXIST;
+
+        // perform a row cleanup
+        $c = $accountRow -> length;
+        for ($i = $c - 1; $i > 0; $i--) {     
+            $row = $accountRow[$i];
+            $cell = $row -> getElementsByTagName("Data");
+
+            // Clear row if username is empty
+            foreach ($cell as $j => $cellData)
+                if ($j === ACCOUNT_PARSE_KEYPOS && empty($cellData -> textContent))
+                    $row -> parentNode -> removeChild($row);
+        }
+
+        // add user
+        $newRowData = Array(
+            "id" => $id,
+            "username" => $username,
+            "password" => $password,
+            "name" => $name,
+            "repass" => 0
+        );
+
+        $newRow = $accountDOM -> createElement("Row");
+        $newRow -> setAttribute("ss:AutoFitHeight", "0");
+
+        foreach ($newRowData as $key => $value) {
+            $cell = $accountDOM -> createElement("Cell");
+            $cell -> setAttribute("ss:StyleID", "s64");
+
+            $data = $accountDOM -> createElement("Data", $value);
+            $data -> setAttribute("ss:Type", in_array(gettype($value), ["integer", "double"]) ? "Number" : "String");
+
+            $cell -> appendChild($data);
+            $newRow -> appendChild($cell);
+        }
+
+        $accountTable -> appendChild($newRow);
+
+        // Update change to parsed data
+        $accountData[$username] = $newRowData;
+
+        if ($accountDOM -> save($_SERVER["DOCUMENT_ROOT"] ."/data/xmldb/acctmp.xml") === false)
+            return USER_ADD_ERROR;
+        
+        rename($_SERVER["DOCUMENT_ROOT"] ."/data/xmldb/acctmp.xml", $_SERVER["DOCUMENT_ROOT"] ."/data/xmldb/account.xml");
+        return USER_ADD_SUCCESS;
+    }
+
+    function deleteUser(String $username) {
+        global $accountDOM;
+        global $accountRow;
+        global $accountHeader;
+
+        foreach ($accountRow as $i => $row) {
+            if ($i === 0)
+                continue;
+                
+            $cell = $row -> getElementsByTagName("Data");
+
+            foreach ($cell as $j => $cellData)
+                if ($j === array_search("username", $accountHeader) && $cellData -> textContent === $username) {
+                    foreach ($replace as $k => $val)
+                        if (($cellIndex = array_search($k, $accountHeader)) !== false)
+                            $row -> parentNode -> removeChild($row);
+
+                    // Update change to parsed data
+                    unset($accountData[$username]);
+
+                    if ($accountDOM -> save($_SERVER["DOCUMENT_ROOT"] ."/data/xmldb/acctmp.xml") === false)
+                        return USER_EDIT_ERROR;
+                        
+                    rename($_SERVER["DOCUMENT_ROOT"] ."/data/xmldb/acctmp.xml", $_SERVER["DOCUMENT_ROOT"] ."/data/xmldb/account.xml");
+                    return USER_EDIT_SUCCESS;
+                }
+        }
+
         return USER_EDIT_WRONGUSERNAME;
     }
