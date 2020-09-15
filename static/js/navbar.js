3a279be0c690051898dbbb480f04bd6d186dde96
Belikhun <belivipro9x99@gmail.com>
2020-09-15 19:16:16 +0700

    🧭 redesign navbar

---
+++ static/js/navbar.js
@@ -0,0 +1,619 @@
+//? |-----------------------------------------------------------------------------------------------|
+//? |  /static/js/navbar.js                                                                         |
+//? |                                                                                               |
+//? |  Copyright (c) 2018-2020 Belikhun. All right reserved                                         |
+//? |  Licensed under the MIT License. See LICENSE in the project root for license information.     |
+//? |-----------------------------------------------------------------------------------------------|
+
+const navbar = {
+	container: HTMLElement.prototype,
+
+	block: {
+		left: HTMLElement.prototype,
+		middle: HTMLElement.prototype,
+		right: HTMLElement.prototype
+	},
+
+	tooltip: {
+		container: HTMLElement.prototype,
+		title: HTMLElement.prototype,
+		description: HTMLElement.prototype
+	},
+
+	subWindow: HTMLElement.prototype,
+	subWindowLists: [],
+
+	init(container) {
+		if (typeof container !== "object" || !container.classList)
+			throw { code: -1, description: `navbar.init(): container is not a valid node` }
+
+		this.container = container;
+		this.container.classList.add("navigationBar");
+		emptyNode(this.container);
+
+		for (let key of Object.keys(this.block)) {
+			this.block[key] = document.createElement("span");
+			this.block[key].classList.add("group", key);
+			this.container.appendChild(this.block[key]);
+		}
+
+		this.tooltip.container = document.createElement("div");
+		this.tooltip.container.classList.add("navTip");
+		this.tooltip.title = document.createElement("t");
+		this.tooltip.title.classList.add("title");
+		this.tooltip.description = document.createElement("t");
+		this.tooltip.description.classList.add("description");
+		this.tooltip.container.append(this.tooltip.title, this.tooltip.description);
+
+		this.container.appendChild(this.tooltip.container);
+	},
+
+	insert(component, location, order) {
+		if (typeof component !== "object" || !component.container)
+			throw { code: -1, description: `navbar.insert(): not a valid component` }
+
+		if (!this.block[location])
+			throw { code: -1, description: `navbar.insert(): not a valid location` }
+
+		this.block[location].appendChild(component.container);
+
+		if (order)
+			component.container.style.order = order;
+	},
+
+	remove(component, location) {
+		if (typeof component !== "object" || !component.container)
+			throw { code: -1, description: `navbar.insert(): not a valid component` }
+
+		if (!this.block[location])
+			throw { code: -1, description: `navbar.insert(): not a valid location` }
+
+		this.block[location].removeChild(component.container);
+	},
+
+	Tooltip: class {
+		constructor(node, {
+			title = null,
+			description = null
+		} = {}) {
+			this.locked = false;
+			this.disabled = false;
+			this.tooltip = navbar.tooltip;
+			this.title = title;
+			this.description = description;
+
+			node.addEventListener("mouseenter", () => {
+				if (this.locked || this.disabled || !this.title || !this.description || node.classList.contains("active"))
+					return;
+
+				let leftPos = node.getBoundingClientRect().left;
+				let rightPos = leftPos + node.offsetWidth;
+
+				if ((rightPos / window.innerWidth) > 0.8) {
+					this.tooltip.container.classList.add("flip");
+					this.tooltip.container.style.left = null;
+					this.tooltip.container.style.right = `${window.innerWidth - rightPos}px`;
+				} else {
+					this.tooltip.container.classList.remove("flip");
+					this.tooltip.container.style.left = `${leftPos}px`;
+					this.tooltip.container.style.right = null;
+				}
+
+				this.tooltip.title.innerText = this.title;
+				this.tooltip.description.innerText = this.description;
+				this.tooltip.container.classList.add("show");
+			});
+
+			node.addEventListener("click", () => {
+				this.tooltip.container.classList.remove("show");
+				this.locked = true;
+			});
+
+			node.addEventListener("mouseleave", () => {
+				this.tooltip.container.classList.remove("show");
+				this.locked = false;
+			});
+		}
+
+		set({
+			title = null,
+			description = null
+		}) {
+			if (title)
+				this.title = title;
+
+			if (description)
+				this.description = description;
+		}
+	},
+
+	SubWindow: class {
+		constructor(container, content = "BLANK", color = "gray") {
+			this.id = randString(6);
+			this.showing = false;
+			this.hideTimeout = null;
+			this.container = container
+
+			this.windowNode = document.createElement("div");
+			this.windowNode.classList.add("subWindow", "hide");
+			this.windowNode.dataset.id = this.id;
+			this.color = color;
+
+			this.contentNode = document.createElement("div");
+			this.contentNode.classList.add("content");
+			this.content = content;
+
+			new ResizeObserver(() => this.update()).observe(this.contentNode);
+	
+			this.windowNode.appendChild(this.contentNode);
+			this.container.appendChild(this.windowNode);
+			navbar.subWindowLists.push(this);
+		}
+
+		update() {
+			let height = (this.showing) ? this.contentNode.offsetHeight : 0;
+			this.windowNode.style.height = `${height}px`;
+			
+			if (this.showing) {
+				let width = this.contentNode.offsetWidth;
+				let leftPos = this.container.getBoundingClientRect().left;
+				let rightPos = leftPos + this.container.offsetWidth;
+				let middlePos = (leftPos + rightPos) / 2;
+	
+				if ((leftPos + width) <= window.innerWidth && middlePos > (width / 2))
+					this.windowNode.dataset.align = "center";
+				else if ((leftPos + width) > window.innerWidth)
+					this.windowNode.dataset.align = "center";
+				else
+					this.windowNode.dataset.align = "left";
+	
+				this.windowNode.style.width = `${width}px`;
+			}
+		}
+
+		show() {
+			clearTimeout(this.hideTimeout);
+
+			for (let item of navbar.subWindowLists)
+				if (item.id !== this.id)
+					item.hide();
+
+			this.windowNode.classList.remove("hide");
+			this.windowNode.classList.add("show");
+			this.container.classList.add("active");
+
+			this.showing = true;
+			this.update();
+		}
+
+		hide() {
+			clearTimeout(this.hideTimeout);
+			this.windowNode.classList.remove("show");
+			this.container.classList.remove("active");
+
+			this.showing = false;
+			this.update();
+
+			this.hideTimeout = setTimeout(() => this.windowNode.classList.add("hide"), 300);
+		}
+
+		toggle() {
+			(this.showing) ? this.hide() : this.show();
+		}
+
+		/**
+		 * @param {string} color	Color
+		 */
+		set color(color) {
+			this.windowNode.dataset.color = color;
+		}
+
+		/**
+		 * @param {String|Element} content
+		 */
+		set content(content) {
+			if (typeof content === "object" && content.classList) {
+				emptyNode(this.contentNode);
+				this.contentNode.appendChild(content);
+			} else
+				this.contentNode.innerHTML = content;
+		}
+	},
+
+	Clickable: class {
+		constructor(container) {
+			if (typeof container !== "object" || !container.appendChild)
+				throw { code: -1, description: `navbar.Clickable(): container is not a valid node` }
+
+			this.container = container;
+			this.container.classList.add("clickable");
+			this.handlers = []
+
+			this.clickBox = document.createElement("div");
+			this.clickBox.classList.add("clickBox");
+			this.clickBox.addEventListener("click", (e) => this.__trigger(e));
+
+			this.container.appendChild(this.clickBox);
+		}
+
+		setHandler(handler) {
+			if (typeof handler !== "function")
+				throw { code: -1, description: `navbar.Clickable.setHandler(): not a valid function` }
+
+			return this.handlers.push(handler);
+		}
+
+		removeHandler(handler) {
+			if (this.handlers[handler])
+				this.handlers[handler] = undefined;
+		}
+
+		__trigger(e) {
+			let isActive = this.container.classList.contains("active");
+
+			if (isActive)
+				this.container.classList.remove("active");
+			else
+				this.container.classList.add("active");
+
+			e.isActive = !isActive;
+
+			for (let item of this.handlers)
+				if (typeof item === "function")
+					item(e);
+		}
+	},
+
+	//? NAVIGATION BAR COMPONENT
+
+	title({
+		icon = "/api/images/icon",
+		background = "/api/images/landing",
+		title = "App Name",
+		description = "App Description"
+	} = {}) {
+		let container = document.createElement("span");
+		container.classList.add("component", "title");
+
+		let iconNode = new lazyload({
+			source: icon,
+			classes: "icon"
+		})
+
+		let titleNode = document.createElement("t");
+		titleNode.classList.add("title");
+		titleNode.innerHTML = title;
+
+		container.append(iconNode.container, titleNode);
+
+		let navtip = new this.Tooltip(container, (arguments && arguments[0] && arguments[0].tooltip) ? arguments[0].tooltip : {})
+		let click = new this.Clickable(container);
+		let subWindow = new this.SubWindow(container);
+
+		let subContainer = document.createElement("div");
+		subContainer.classList.add("contestDetails");
+
+		let subWinHeader = document.createElement("div");
+		subWinHeader.classList.add("header");
+
+		let subWinBackground = new lazyload({
+			source: background,
+			classes: "background"
+		})
+
+		let subWinIcon = new lazyload({
+			source: icon,
+			classes: "icon"
+		})
+
+		let subWinTitle = document.createElement("t");
+		subWinTitle.classList.add("title");
+		subWinTitle.innerText = title;
+
+		let subWinDescription = document.createElement("t");
+		subWinDescription.classList.add("description");
+		subWinDescription.innerHTML = description;
+
+		subWinHeader.append(subWinBackground.container, subWinIcon.container, subWinTitle);
+		subContainer.append(subWinHeader, subWinDescription);
+		subWindow.content = subContainer;
+		click.setHandler(() => subWindow.toggle());
+
+		return {
+			container,
+			subWindow,
+			tooltip: navtip,
+			click,
+
+			set({
+				icon = null,
+				background = null,
+				title = null,
+				description = null,
+			}) {
+				if (icon)
+					subWinIcon.src = iconNode.src = icon;
+
+				if (background)
+					subWinBackground.src = background;
+
+				if (title)
+					subWinTitle.innerText = titleNode.innerText = title;
+
+				if (description)
+					subWinDescription.innerHTML = description;
+			}
+		}
+	},
+
+	iconButton({
+		icon = "circle"
+	}) {
+		let container = document.createElement("span");
+		container.classList.add("component", "iconBtn");
+
+		let iconNode = document.createElement("span");
+		iconNode.classList.add("icon");
+		iconNode.dataset.icon = icon;
+
+		container.appendChild(iconNode);
+
+		let navtip = new this.Tooltip(container, (arguments && arguments[0] && arguments[0].tooltip) ? arguments[0].tooltip : {})
+		let click = new this.Clickable(container);
+
+		return {
+			container,
+			tooltip: navtip,
+			click,
+
+			set({
+				icon = null
+			}) {
+				if (icon)
+					iconNode.dataset.icon = icon;
+			}
+		}
+	},
+
+	menuButton({} = {}) {
+		let container = document.createElement("span");
+		container.classList.add("component", "menuBtn");
+
+		let iconNode = document.createElement("span");
+		iconNode.classList.add("hamburger");
+		iconNode.innerHTML = `<span></span><span></span><span></span>`
+
+		container.appendChild(iconNode);
+
+		let navtip = new this.Tooltip(container, (arguments && arguments[0] && arguments[0].tooltip) ? arguments[0].tooltip : {})
+		let click = new this.Clickable(container);
+
+		return {
+			container,
+			tooltip: navtip,
+			click,
+
+			set({
+				icon = null
+			}) {
+				if (icon)
+					iconNode.dataset.icon = icon;
+			}
+		}
+	},
+
+	account({
+		name = "Guest",
+		avatar = "/api/avatar",
+		color = "brown"
+	}) {
+		let container = document.createElement("span");
+		container.classList.add("component", "account");
+
+		triBg(container, { color, scale: .4 });
+
+		let avatarNode = new lazyload({
+			source: avatar,
+			classes: "avatar"
+		})
+
+		let nameNode = document.createElement("t");
+		nameNode.classList.add("name");
+		nameNode.innerHTML = name;
+
+		container.append(avatarNode.container, nameNode);
+
+		let navtip = new this.Tooltip(container, (arguments && arguments[0] && arguments[0].tooltip) ? arguments[0].tooltip : {})
+		let click = new this.Clickable(container);
+		let subWindow = new this.SubWindow(container);
+		click.setHandler(() => subWindow.toggle());
+
+		return {
+			container,
+			subWindow,
+			tooltip: navtip,
+			click,
+
+			set({
+				name = null,
+				avatar = null,
+				color = null
+			}) {
+				if (name)
+					nameNode.innerHTML = name;
+
+				if (avatar)
+					avatarNode.src = avatar;
+
+				if (color)
+					this.container.dataset.triColor = color;
+			}
+		}
+	},
+
+	announcement({
+		level = null,
+		message = null,
+		time = null,
+	} = {}) {
+		let container = document.createElement("span");
+		container.classList.add("component", "announcement", "hide");
+
+		let main = document.createElement("div");
+		main.classList.add("main");
+
+		let icon = document.createElement("span");
+		icon.classList.add("icon");
+
+		let levelText = document.createElement("t");
+		levelText.classList.add("level");
+
+		let wrapper = document.createElement("span");
+		wrapper.classList.add("wrapper");
+
+		let content = document.createElement("t");
+		content.classList.add("content");
+
+		wrapper.appendChild(content);
+		main.append(icon, levelText, wrapper);
+		container.appendChild(main);
+
+		let navtip = new this.Tooltip(container, (arguments && arguments[0] && arguments[0].tooltip) ? arguments[0].tooltip : {})
+		let click = new this.Clickable(container);
+		let subWindow = new this.SubWindow(container);
+		let subNodes = buildElementTree("div", "announcementDetail", [
+			{ type: "span", class: "icon", name: "icon" },
+			{ type: "t", class: "level", name: "level" },
+			{ type: "t", class: "detail", name: "detail" },
+			{ type: "div", class: "footer", name: "footer", list: [
+				{ type: "span", class: "icon", name: "icon" },
+				{ type: "t", class: "time", name: "time" },
+
+				{ type: "button", class: ["sq-btn", "blue", "read"], name: "read", text: "Đánh Dấu Đã Đọc" }
+			]}
+		])
+
+		subWindow.content = subNodes.tree;
+		subNodes = subNodes.obj;
+
+		//> CONTROL
+		let animationTimeout = null;
+
+		let levelDetail = {
+			okay: "Hoàn Thành",
+			info: "Thông Báo",
+			warning: "Cảnh Báo",
+			error: "Lỗi"
+		}
+
+		const show = () => {
+			// RESET
+			clearTimeout(animationTimeout);
+			wrapper.removeAttribute("style");
+			content.removeAttribute("style");
+			levelText.removeAttribute("style");
+			container.classList.remove("hide", "detail");
+			requestAnimationFrame(() => container.classList.add("show"));
+
+			animationTimeout = setTimeout(() => {
+				levelText.style.width = `${levelText.scrollWidth}px`;
+				levelText.style.opacity = 1;
+				levelText.style.paddingLeft = `10px`;
+
+				animationTimeout = setTimeout(() => {
+					levelText.removeAttribute("style");
+					container.classList.add("detail");
+					subWindow.update()
+					
+					if (content.scrollWidth < 200)
+						wrapper.style.width = `${content.scrollWidth}px`;
+					else
+						animationTimeout = setTimeout(() => {
+							let duration = Math.max(content.innerText.split(" ").length * 0.6, 5);
+							content.style.animationName = "announcement-scrolling";
+							content.style.animationDuration = `${duration}s`;
+
+							animationTimeout = setTimeout(() => {
+								wrapper.removeAttribute("style");
+								content.removeAttribute("style");
+
+								levelText.style.width = `${levelText.scrollWidth}px`;
+								levelText.style.opacity = 1;
+								levelText.style.paddingLeft = `10px`;
+								content.style.display = "none";
+								container.classList.remove("detail");
+
+								animationTimeout = setTimeout(() => subWindow.update(), 500);
+							}, duration * 1000)
+						}, 2000);
+				}, 1000)
+			}, 900)
+		}
+
+		const hide = () => {
+			clearTimeout(animationTimeout);
+			wrapper.removeAttribute("style");
+			content.removeAttribute("style");
+			levelText.removeAttribute("style");
+			container.removeAttribute("data-level");
+			container.classList.remove("detail");
+			container.classList.remove("show");
+
+			animationTimeout = setTimeout(() => container.classList.add("hide"), 1000);
+		}
+
+		const set = ({
+			level = null,
+			message = null,
+			time = null
+		} = {}) => {
+			if (level) {
+				subNodes.level.innerText = levelText.innerText = levelDetail[level] || level;
+				subNodes.dataset.level = container.dataset.level = level;
+				subWindow.color = {
+					"okay": "green",
+					"info": "gray",
+					"warning": "yellow",
+					"error": "red"
+				}[level] || "gray";
+			}
+	
+			if (message) {
+				subNodes.detail.innerHTML = message;
+
+				//? A Workaround to Allow <b> tag to display in preview mode only
+				message = message
+					.replace(/<b>/g, "/**")
+					.replace(/<\/b>/g, "**\\");
+
+				content.innerHTML = sanitizeHTML(message)
+					.replace(/\/\*\*/g, "<b>")
+					.replace(/\*\*\\/g, "</b>");
+			}
+
+			subNodes.footer.time.innerText = (time) ? (new Date(time * 1000)).toLocaleString() : "Không Rõ";
+
+			if (level && message)
+				show();
+		}
+
+		subNodes.footer.read.addEventListener("click", () => {
+			hide();
+			subWindow.hide();
+		})
+
+		click.setHandler(() => subWindow.toggle());
+		set({ level, message, time });
+
+		return {
+			container,
+			subWindow,
+			tooltip: navtip,
+			click,
+
+			show,
+			hide,
+			set
+		}
+	}
+}
