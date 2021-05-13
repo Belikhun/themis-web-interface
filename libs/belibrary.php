1ca1a21c48e0f926edacfac9651c63d5fcdbbe14
Belikhun <belivipro9x99@gmail.com>
2021-05-13 09:52:01 +0700

    🎲 config: fix deserialization error

--- libs/belibrary.php
+++ libs/belibrary.php
@@ -780,7 +780,7 @@
 					// Temporary disable `NOTICE` error reporting
 					// to try unserialize data without triggering `E_NOTICE`
 					try {
-						error_reporting(null);
+						error_reporting(0);
 						$data = unserialize($data);
 						error_reporting(E_ALL);
 					} catch(Throwable $e) {
